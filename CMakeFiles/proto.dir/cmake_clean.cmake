433d611e9a1714fbea7389bf7866fef5b7b5460a
FyS <ballandFyS@gmail.com>
2017-12-21 12:05:54 +0100

    init

---
+++ CMakeFiles/proto.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "FySLoginMessage.pb.cc"
+  "FySLoginMessage.pb.h"
+  "FySBabbleMessage.pb.cc"
+  "FySBabbleMessage.pb.h"
+  "FySGtwMessage.pb.cc"
+  "FySGtwMessage.pb.h"
+  "FySAuthenticationResponse.pb.cc"
+  "FySAuthenticationResponse.pb.h"
+  "CMakeFiles/proto.dir/FySLoginMessage.pb.cc.o"
+  "CMakeFiles/proto.dir/FySBabbleMessage.pb.cc.o"
+  "CMakeFiles/proto.dir/FySGtwMessage.pb.cc.o"
+  "CMakeFiles/proto.dir/FySAuthenticationResponse.pb.cc.o"
+  "libproto.pdb"
+  "libproto.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
